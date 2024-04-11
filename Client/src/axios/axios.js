4a0e7ba0cea404268f4f05df950969a5f15dca5a
Ennygenius <inuenike@gmail.com>
2024-04-11 09:01:42 -0700

    added axios url

--- Client/src/axios/axios.js
+++ Client/src/axios/axios.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
-const URI = "http://127.0.0.1:2005/";
+const URI = "https://expressglobaluk.onrender.com/";
+// const URI = "http://127.0.0.1:2005/";
 
 export const Base = axios.create({
   baseURL: `${URI}`,
